ecfd3cea14e4e5af2b672789b57f8ae489b24424
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-05-31 17:23:17 -0300

    GTPSE:36: Implement the Business Logic

--- modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/internal/resource/v1_0/AccountResourceImpl.java
+++ modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/internal/resource/v1_0/AccountResourceImpl.java
@@ -1,11 +1,13 @@
 package com.liferay.amf.internal.resource.v1_0;
 
 import com.liferay.amf.resource.v1_0.AccountResource;
-import com.liferay.amf.model.Account;
+import com.liferay.amf.dto.v1_0.Account;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  * @author me
  */
@@ -13,6 +15,27 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v1_0/account.properties",
 	scope = ServiceScope.PROTOTYPE, service = AccountResource.class
 )
-public class AccountResourceImpl extends BaseAccountResourceImpl {
+public class AccountResourceImpl extends BaseAccountResourceImpl   {
+
+	Map<Integer, Account> account = new HashMap<>();
+
+	public Account getAccount(Integer accountId) throws Exception {
+		return getAccount(accountId);
+	}
+
+	public void deleteAccount(Integer accountId) throws Exception {
+		deleteAccount(accountId);
+
+	}
+
+	public Account createAccount(Account account) throws Exception {
+
+		return createAccount(account);
+	}
+
+	public Account updateAccount(Integer accountId, Account account) throws Exception {
+
+		return updateAccount(accountId,account);
+	}
 
 }
