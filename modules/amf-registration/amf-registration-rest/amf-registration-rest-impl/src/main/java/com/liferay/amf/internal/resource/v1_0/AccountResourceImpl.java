0410bf65e9e618042ab16db27fefe4b0016156c7
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-01 15:56:42 -0300

    GTPSE:36: fix business logic methods

--- modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/internal/resource/v1_0/AccountResourceImpl.java
+++ modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/internal/resource/v1_0/AccountResourceImpl.java
@@ -3,13 +3,9 @@ package com.liferay.amf.internal.resource.v1_0;
 import com.liferay.amf.dto.v1_0.Account;
 import com.liferay.amf.resource.v1_0.AccountResource;
 import com.liferay.amf.service.AccountLocalService;
-import com.liferay.portal.vulcan.pagination.Page;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
-
-import java.util.Collections;
-import java.util.Date;
 
 /**
  * @author me
@@ -21,33 +17,34 @@ import java.util.Date;
 public class AccountResourceImpl extends BaseAccountResourceImpl {
 
 	@Override
-	public Account getAccount(String accountId, Account account) throws Exception {
+	public Account getAccount(String accountId) throws Exception {
 
-		return _accountLocalService.getAccount(accountId, account.getFirstName(), account.getLastName(), account.getEmailAdress(),
-				account.getUserName(), account.getGenre(), account.getBirthday(), account.getBirthday(), account.getPassword(), account.getHomePhone(),
-				account.getMobilePhone(), account.getAdress1(),account.getAdress2(), account.getCity(), account.getState(), account.getSecurityQuestion(),
-				account.getSecurityAnswer(), account.getacceptedTou);
+		return (Account) _accountLocalService.getAccount(accountId);
 	}
 
-	@Override
+
 	public Account createAccount(Account account) throws Exception {
 
-		return _accountLocalService.createAccount(account.getFirstName(), account.getLastName(), account.getEmailAdress(),
-				account.getUserName(), account.getGenre(), account.getBirthday(), account.getBirthday(), account.getPassword(), account.getHomePhone(),
-				account.getMobilePhone(), account.getAdress1(),account.getAdress2(), account.getCity(), account.getState(), account.getSecurityQuestion(),
-				account.getSecurityAnswer(), account.getacceptedTou);
+		return (Account) _accountLocalService.createAccount(account.getFirstName(), account.getLastName(),
+				account.getEmailAdress(), account.getAccountName(), account.getGenre(), account.getBirthday(),
+				account.getPassword(), account.getHomePhone(), account.getMobilePhone(), account.getAdress1(),
+				account.getAdress2(), account.getCity(), account.getState(), account.getSecurityQuestion(),
+				account.getSecurityAnswer());
 	}
+
 	@Override
 	public void deleteAccount(String accountId) throws Exception {
 		_accountLocalService.deleteAccount(accountId);
 	}
-	@Override
+
+
 	public Account updateAccount(String accountId, Account account) throws Exception {
 
-		return _accountLocalService.updateAccount(accountId, account.getFirstName(), account.getLastName(), account.getEmailAdress(),
-				account.getUserName(), account.getGenre(), account.getBirthday(), account.getBirthday(), account.getPassword(), account.getHomePhone(),
-				account.getMobilePhone(), account.getAdress1(),account.getAdress2(), account.getCity(), account.getState(), account.getSecurityQuestion(),
-				account.getSecurityAnswer(), account.getacceptedTou);
+		return (Account) _accountLocalService.updateAccount(accountId, account.getFirstName(), account.getLastName(),
+				account.getEmailAdress(), account.getAccountName(), account.getGenre(), account.getBirthday(),
+				account.getPassword(), account.getHomePhone(), account.getMobilePhone(), account.getAdress1(),
+				account.getAdress2(), account.getCity(), account.getState(), account.getSecurityQuestion(),
+				account.getSecurityAnswer());
 		}
 	@Reference
 	private AccountLocalService _accountLocalService;
