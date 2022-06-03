dcd0f146b120cd86054ae2e7e3a27c879e102664
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-03 09:41:52 -0300

    GTPSE-48: create web module

---
+++ modules/amf-registration-web/src/main/java/com/liferay/amf/constants/AmfRegistrationWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.constants;
+
+/**
+ * @author me
+ */
+public class AmfRegistrationWebPortletKeys {
+
+	public static final String AMFREGISTRATIONWEB =
+		"com_liferay_amf_AmfRegistrationWebPortlet";
+
+}
---
+++ modules/amf-registration-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="amfregistrationweb.caption"/></b>
+</p>
