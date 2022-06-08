236d1ffe503c286d1ea398cc6c24444303e610fe
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-08 18:15:33 -0300

    GSTPE-36-Rest-Module: Connect with service leayer

--- modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/rest/internal/resource/v1_0/AccountResourceImpl.java
+++ modules/amf-registration/amf-registration-rest/amf-registration-rest-impl/src/main/java/com/liferay/amf/rest/internal/resource/v1_0/AccountResourceImpl.java
@@ -1,8 +1,11 @@
 package com.liferay.amf.rest.internal.resource.v1_0;
 
+import com.liferay.amf.rest.dto.v1_0.Account;
 import com.liferay.amf.rest.resource.v1_0.AccountResource;
 
+import com.liferay.amf.service.AccountLocalService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -13,4 +16,32 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = AccountResource.class
 )
 public class AccountResourceImpl extends BaseAccountResourceImpl {
+
+	@Override
+	public Account createAccount(Account account) throws Exception {
+		
+		_accountLocalService.createAccount(
+			account.getFirstName(),
+			account.getLastName(),
+			account.getEmailAddress(),
+			account.getAccountName(),
+			account.getGenre(),
+			account.getBirthday(),
+			account.getPassword(),
+			account.getHomePhone(),
+			account.getMobilePhone(),
+			account.getAddress1(),
+			account.getAddress2(),
+			account.getCity(),
+			account.getState(),
+			account.getSecurityQuestion(),
+			account.getSecurityAnswer()
+		);
+
+		return account;
+	}
+	
+	@Reference
+	private AccountLocalService _accountLocalService;
+
 }
