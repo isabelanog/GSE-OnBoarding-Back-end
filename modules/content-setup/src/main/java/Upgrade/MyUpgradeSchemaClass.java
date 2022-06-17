0c44df55facd4226b629b055a9680bb2c15a6dd2
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-17 11:24:28 -0300

    Creating Upgrade Processes for Modules

---
+++ modules/content-setup/src/main/java/Upgrade/MyUpgradeSchemaClass.java
@@ -0,0 +1,10 @@
+package Upgrade;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+public class MyUpgradeSchemaClass extends UpgradeProcess {
+    @Override
+    protected void doUpgrade() throws Exception {
+
+    }
+}
---
+++ modules/content-setup/src/main/java/content/setup/api/ContentSetup.java
@@ -0,0 +1,7 @@
+package content.setup.api;
+
+/**
+ * @author me
+ */
+public interface ContentSetup {
+}
