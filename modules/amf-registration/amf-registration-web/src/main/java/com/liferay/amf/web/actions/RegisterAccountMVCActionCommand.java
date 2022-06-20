761fd36cad020f89cd283633fdfc31f42fed62aa
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-20 14:25:23 -0300

    GSTPE-48: MVC actions command

--- modules/amf-registration/amf-registration-web/src/main/java/com/liferay/amf/web/actions/RegisterAccountMVCActionCommand.java
+++ modules/amf-registration/amf-registration-web/src/main/java/com/liferay/amf/web/actions/RegisterAccountMVCActionCommand.java
@@ -10,6 +10,7 @@ import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import java.util.Date;
 
 @Component(
         immediate = true,
@@ -22,18 +23,16 @@ import javax.portlet.ActionResponse;
 public class RegisterAccountMVCActionCommand extends BaseMVCActionCommand {
     @Override
     protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
-
-        //Get parameters from the request
-
+        Long accountId = ParamUtil.getLong(actionRequest, "accountId")
         String firstName = ParamUtil.getString(actionRequest, "firstName");
         String lastName = ParamUtil.getString(actionRequest, "lastName");
         String emailAddress = ParamUtil.getString(actionRequest, "emailAddress");
         String accountName = ParamUtil.getString(actionRequest, "accountName");
         String gender = ParamUtil.getString(actionRequest, "gender");
-        String birthday = ParamUtil.getString(actionRequest, "birthday");
+        Date birthday = ParamUtil.getDate(actionRequest, "birthday");
         String password = ParamUtil.getString(actionRequest, "password");
-        String homePhone = ParamUtil.getString(actionRequest, "homePhone");
-        String  mobilePhone = ParamUtil.getString(actionRequest, "mobilePhone");
+        int homePhone = Integer.parseInt(ParamUtil.getString(actionRequest, "homePhone"));
+        int  mobilePhone = Integer.parseInt(ParamUtil.getString(actionRequest, "mobilePhone"));
         String address = ParamUtil.getString(actionRequest, "address");
         String address2 = ParamUtil.getString(actionRequest, "address2");
         String city = ParamUtil.getString(actionRequest, "city");
@@ -42,12 +41,11 @@ public class RegisterAccountMVCActionCommand extends BaseMVCActionCommand {
         String securitysecurityAnswer = ParamUtil.getString(actionRequest, "securityAnswer");
         String acceptedTou = ParamUtil.getString(actionRequest, "acceptedTou");
 
-       //Call the service to add a new Account
-        _accountLocalService.createAccount(firstName,lastName,emailAddress,accountName,gender,birthday,password,
-                homePhone,mobilePhone,address,address2,city,statezip,securityQuestion,securitysecurityAnswer,
-                acceptedTou);
 
-          }
+        _accountLocalService.createAccount(firstName,lastName,emailAddress,accountName,gender,birthday,password,
+                homePhone, mobilePhone,address,address2,city,statezip,securityQuestion,securitysecurityAnswer,
+                acceptedTou);
+    }
     @Reference
     private AccountLocalService _accountLocalService;
 }
