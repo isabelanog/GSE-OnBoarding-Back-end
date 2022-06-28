e417b0a34b80e09d71579ff4c2100a37d9ebb229
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-06-28 16:07:58 -0300

    GSTPE-72: Upgrade process class

--- modules/content-setup/src/main/java/com/liferay/content/setup/upgrade/CreateSampleSiteUpgradeProcess.java
+++ modules/content-setup/src/main/java/com/liferay/content/setup/upgrade/CreateSampleSiteUpgradeProcess.java
@@ -4,38 +4,32 @@ import com.liferay.content.setup.constants.SampleSiteSetupConstants;
 import com.liferay.content.setup.helper.SampleSiteSetupHelper;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import org.osgi.service.component.annotations.Reference;
 
 public class CreateSampleSiteUpgradeProcess extends UpgradeProcess {
 
-    public CreateSampleSiteUpgradeProcess(CreateSampleSiteUpgradeProcess createSampleSiteUpgradeProcess){
-
-    }
-
     @Override
     protected void doUpgrade() throws Exception {
 
-        Group site = _sampleSiteSetupHelper.addSite(
+        Group sampleSite = _sampleSiteSetupHelper.addSite(
                 SampleSiteSetupConstants.SAMPLE_PAGE_NAME,
                 SampleSiteSetupConstants.SAMPLE_PAGE_DESCRIPTION,
                 SampleSiteSetupConstants.SAMPLE_PAGE_URL,
                 GroupConstants.TYPE_SITE_OPEN);
 
         _sampleSiteSetupHelper.addPage(
-                site.getGroupId(),
+                sampleSite.getGroupId(),
                 false,
-                GroupConstants.DEFAULT_PARENT_GROUP_ID,
+                LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
                 SampleSiteSetupConstants.HIDDEN_PAGE_NAME,
                 SampleSiteSetupConstants.HIDDEN_PAGE_TITLE,
                 SampleSiteSetupConstants.HIDDEN_PAGE_DESCRIPTION,
-                GroupConstants.TYPE_SITE_OPEN,
+                LayoutConstants.TYPE_CONTENT,
                 true,
                 SampleSiteSetupConstants.HIDDEN_PAGE_URL,
                 SampleSiteSetupConstants.LAYOUT_1_COLUMN);
-
-
-
     }
     @Reference
     private SampleSiteSetupHelper _sampleSiteSetupHelper;
