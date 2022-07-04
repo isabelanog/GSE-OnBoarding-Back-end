f8b2edfcaa37b4e04b89365405928133e68bc84a
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-07-04 17:03:10 -0300

    GSTPE-80: fix SampleSite URL

--- modules/content-setup/src/main/java/com/liferay/content/setup/constants/SampleSiteSetupConstants.java
+++ modules/content-setup/src/main/java/com/liferay/content/setup/constants/SampleSiteSetupConstants.java
@@ -2,14 +2,19 @@ package com.liferay.content.setup.constants;
 
 public class SampleSiteSetupConstants {
 
-    public static final String SAMPLE_PAGE_NAME = "Sample Site" ;
+    public static final String SAMPLE_SITE_NAME = "Sample Site";
 
-    public static final String SAMPLE_PAGE_DESCRIPTION = "Sample Site Description";
+    public static final String SAMPLE_SITE_DESCRIPTION = "Sample Site Description";
 
-    public static final String SAMPLE_PAGE_URL = "/samplesite";
+    public static final String SAMPLE_SITE_URL = "/samplesite";
 
-    public static final String BUNDLE_SYMBOLIC_NAME = "com.liferay.content.setup";
+    public static final String SAMPLE_PAGE_NAME = "Sample Page Name";
 
+    public static final String SAMPLE_PAGE_TITLE = "Sample Page Title";
+
+    public static final String SAMPLE_PAGE_DESCRIPTION = "Sample Page Description";
+
+    public static final String SAMPLE_PAGE_URL = "/samplepage";
     public static final String LAYOUT_1_COLUMN = "1_column";
 
     public static final String HIDDEN_PAGE_NAME = "Page Hidden";
@@ -19,7 +24,5 @@ public class SampleSiteSetupConstants {
     public static final String HIDDEN_PAGE_DESCRIPTION = "The requested resource could not be found.";
 
     public static final String HIDDEN_PAGE_URL = "/hiddenpage";
-
-
 
 }
