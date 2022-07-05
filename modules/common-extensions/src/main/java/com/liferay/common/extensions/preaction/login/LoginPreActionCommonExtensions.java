3e5214446cd32e50e6cd70f03c6151b9c9ac5484
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-07-05 14:38:14 -0300

    GSTPE-80: FormatSource

--- modules/common-extensions/src/main/java/com/liferay/common/extensions/preaction/login/LoginPreActionCommonExtensions.java
+++ modules/common-extensions/src/main/java/com/liferay/common/extensions/preaction/login/LoginPreActionCommonExtensions.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.common.extensions.preaction.login;
 
 import com.liferay.portal.kernel.events.ActionException;
@@ -8,45 +22,57 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.WebKeys;
+
 import constants.CommonExtensionConstantsPreAction;
-import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
+import org.osgi.service.component.annotations.Component;
+
+/**
+ @author Isabela Nogueira
+ */
 @Component(
-        immediate = true,
-        property = {
-            "key=" + PropsKeys.SERVLET_SERVICE_EVENTS_PRE
-        },
-        service = LifecycleAction.class
+	immediate = true, property = "key=" + PropsKeys.SERVLET_SERVICE_EVENTS_PRE,
+	service = LifecycleAction.class
 )
 public class LoginPreActionCommonExtensions implements LifecycleAction {
 
-    @Override
-    public void processLifecycleEvent(LifecycleEvent lifecycleEvent) throws ActionException {
+	@Override
+	public void processLifecycleEvent(LifecycleEvent lifecycleEvent)
+		throws ActionException {
 
-        HttpServletRequest request = lifecycleEvent.getRequest();
+		HttpServletRequest request = lifecycleEvent.getRequest();
 
-        ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
 
-        if (!themeDisplay.isSignedIn()) {
+		if (!themeDisplay.isSignedIn()) {
+			String urlCurrent = themeDisplay.getURLCurrent();
 
-                if(themeDisplay.getURLCurrent().contains(CommonExtensionConstantsPreAction.SAMPLE_PAGE_URL)) {
+			if (urlCurrent.contains(
+					CommonExtensionConstantsPreAction.SAMPLE_PAGE_URL)) {
 
-                    HttpServletResponse response = lifecycleEvent.getResponse();
+				HttpServletResponse response = lifecycleEvent.getResponse();
 
-                    try {
-                        response.sendRedirect("/web/samplesite" + CommonExtensionConstantsPreAction.HIDDEN_PAGE_URL);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    _log.info("Guest user attempted to access the sample page");
-            }
-    }
+				try {
+					response.sendRedirect(
+						"/web/samplesite" +
+							CommonExtensionConstantsPreAction.HIDDEN_PAGE_URL);
+				}
+				catch (IOException ioException) {
+					throw new RuntimeException(ioException);
+				}
 
-}
-    private static final Log _log = LogFactoryUtil.getLog(LoginPreActionCommonExtensions.class);
+				_log.info("Guest user attempted to access the sample page");
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LoginPreActionCommonExtensions.class);
 
 }
