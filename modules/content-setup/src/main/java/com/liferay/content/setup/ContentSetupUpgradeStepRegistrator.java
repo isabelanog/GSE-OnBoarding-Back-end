fd08f1a0a0c5ae8349de811175c7e2b8361a10b2
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-07-15 15:24:49 -0300

    fix the schema version (line 21)

--- modules/content-setup/src/main/java/com/liferay/content/setup/ContentSetupUpgradeStepRegistrator.java
+++ modules/content-setup/src/main/java/com/liferay/content/setup/ContentSetupUpgradeStepRegistrator.java
@@ -4,6 +4,7 @@ package com.liferay.content.setup;
 import com.liferay.content.setup.helper.ObjectHelper;
 import com.liferay.content.setup.helper.SampleSiteSetupHelper;
 import com.liferay.content.setup.upgrade.CreateObjectUpgradeProcess;
+import com.liferay.content.setup.upgrade.CreateSampleSiteUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import org.osgi.service.component.annotations.Component;
@@ -16,8 +17,8 @@ public class ContentSetupUpgradeStepRegistrator implements UpgradeStepRegistrato
     public void register(Registry registry) {
 
         registry.register("0.0.0","1.0.0", new DummyUpgradeProcess());
-        //registry.register("1.0.0","1.1.0", new CreateSampleSiteUpgradeProcess(_sampleSiteSetupHelper));
-        registry.register("1.0.0","1.1.0", new CreateObjectUpgradeProcess(_objectHelper));
+        registry.register("1.0.0","1.1.0", new CreateSampleSiteUpgradeProcess(_sampleSiteSetupHelper));
+        registry.register("1.1.0","1.2.0", new CreateObjectUpgradeProcess(_objectHelper));
     }
 
     @Reference
